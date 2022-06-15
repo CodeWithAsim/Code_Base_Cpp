e163b0f02debc6af077c5eaec99e450d9d462a3c
Asim <asimafzal369@gmail.com>
2022-06-16 01:37:25 +0500

    Practicing Getline vs Cin>>

---
+++ Practicing_Getline/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <cmath>
+
+#define MAX_LEN 10
+
+using namespace std ;
+
+int main()
+{
+	
+	
+//	char ch[MAX_LEN] ;
+//	cout<<"Enter Your name : ";
+//	cin.getline(ch,MAX_LEN,'!');
+//	cout<<"You entered this : "<<ch;
+
+
+	
+//	string stat =  "I am a good boy and Alhamdulillah also Muslim";
+//	stringstream ss ;
+//	ss<<stat ;	
+//	string word ;
+//	while(getline(ss,word,' '))
+//	{
+//		cout<<word<<endl;
+//	}
+	
+	
+	
+//	string str ;
+//	getline(cin,str);
+//	cout<<str;
+
+
+
+//	string s = "13.45" ;
+//	int i = stoi(s);	
+//	cout<<i<<endl;
+//	size_t found ;
+//	string s = "hello world";
+//	found = s.find_first_of("aeiou");
+//	while(found!=string::npos)
+//	{
+//		s[found]='*';
+//		found = s.find_first_of("aeiou",found+1);
+//	}	
+//	cout<<s<<endl;
+
+
+
+//	string s = "geeksforgeeks";
+//	string::iterator it ;
+//	string::reverse_iterator rit ;
+//	
+//	for(it=s.begin();it!=s.end();it++)
+//	{
+//		cout<<*it;
+//	}
+//	
+//	cout<<"\n\n\nReverse String Display : \n";
+//	
+//	for(rit=s.rbegin();rit!=s.rend();rit++)
+//	{
+//		cout<<*rit;
+//	}
+
+
+
+//	int number = 100 ; 
+//	stringstream ss ;
+//	ss<<100;
+//	string res ;
+//	ss>>res;
+//	
+//	cout<<res+" ! ";
+
+//	double i = 12.8;
+//	cout<<round(i);
+
+
+
+//	string str = "hello world";
+//	stringstream ss(str);
+//	string word ;
+//	while(ss>>word)
+//	{
+//		cout<<word<<" "; 
+//	}
+
+
+
+//	string str ;
+//	string str2;
+//	cin >> str;
+//	cin>>str2;
+//	cout<<str;
+//	cout<<str2;
+	
+	return 0;	
+} 
